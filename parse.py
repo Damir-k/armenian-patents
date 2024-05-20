8e85fbfc68ff5853545685377ae44a380c224c96
Damir <damir@t1c.ru>
2024-05-21 01:41:19 +0300

    parsed every fmad, updated json (700 entries!)

--- parse.py
+++ parse.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import itertools
 import json
+import datetime
+from tqdm import tqdm
+
 
 def get_table_by_fmad(fmad='01-01', lang='en'):
     if lang not in ['en', 'ru', 'hy']:
@@ -25,17 +28,12 @@ def get_table_by_fmad(fmad='01-01', lang='en'):
     )
     return response
 
-
-
-
-def main():
-    response = get_table_by_fmad("01-01", lang="en")
-    # with open("test.html", "w") as f:
-    #     f.write(response.text)
+def get_overview_by_fmad(fmad='01-01', lang='en'):
+    response = get_table_by_fmad(fmad, lang)
     soup = BeautifulSoup(response.text, features="html.parser")
     
     overview = {
-        "FMAD": '01-01',
+        "FMAD": fmad,
         "data": []
     }
     for (application_id, title, certificate_id) in itertools.batched(soup.find_all("td"), n=3):
@@ -45,8 +43,29 @@ def main():
             "title": title.get_text(),
             "patent_link": 'https://old.aipa.am/search_mods/industrial_design/view_item.php?id=' + certificate_id.get_text() + '&language=en'
         })
+    return overview
+
+def generate_fmads():
+    fmads = []
+    for i in range(33): # 32 - amount of classes in fmad (mkpo, icid)
+        for j in range(17): # 16 - maximum second index in every class
+            fmads.append(str(i).zfill(2) + '-' + str(j).zfill(2))
+    return fmads
+
+def main():
+    certificate_ids = {
+        "parsing_date": datetime.date.today().isoformat(),
+        "entries": 0,
+        "overviews": []
+    }
+    for fmad in tqdm(generate_fmads()):
+        overview = get_overview_by_fmad(fmad, 'en')
+        if overview['data']:
+            certificate_ids["entries"] += len(overview["data"])
+            certificate_ids['overviews'].append(overview)
+
     with open("./data/en/data.json", "w", encoding='utf-8') as f:
-        f.write(json.dumps(overview, ensure_ascii=False))
+        f.write(json.dumps(certificate_ids, ensure_ascii=False))
         
 
 
