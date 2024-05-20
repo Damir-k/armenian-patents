80542270bc0e554569411d6ba9fa865be807e5ee
Damir <damir@t1c.ru>
2024-05-20 23:42:34 +0300

    parses data from the table to json file

--- parse.py
+++ parse.py
@@ -1,4 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
+import itertools
+import json
 
 def main():
     response = requests.request(
@@ -19,6 +22,23 @@ def main():
     )
     with open("test.html", "w") as f:
         f.write(response.text)
+    
+    soup = BeautifulSoup(response.text, features="html.parser")
+    
+    overview = {
+        "FMAD": '01-01',
+        "data": []
+    }
+    for (application_id, title, certificate_id) in itertools.batched(soup.find_all("td"), n=3):
+        overview['data'].append({
+            "certificate_id": certificate_id.get_text(),
+            "application_id": application_id.get_text(),
+            "title": title.get_text(),
+            "patent_link": 'https://old.aipa.am/search_mods/industrial_design/view_item.php?id=' + certificate_id.get_text() + '&language=en'
+        })
+    with open("./data/en/data.json", "w") as f:
+        f.write(json.dumps(overview))
+        
 
 
 
