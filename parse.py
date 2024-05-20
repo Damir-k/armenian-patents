9d71f13d4bd3157c0d84285f29188c91af977a1a
Damir <damir@t1c.ru>
2024-05-20 22:19:40 +0300

    got a table of patents from api by FMAD code

--- parse.py
+++ parse.py
@@ -1,6 +1,25 @@
+import requests
 
 def main():
-    pass
+    response = requests.request(
+        'post',
+        'https://aipo.am/en//ajax/search_mods_search_int_classification',
+        data={
+            'logic': 'partial',
+            'FMAD': '01-01',
+
+            'Reg_num':'',
+            'App_num':'',
+            'App_date':'',
+            'name':'',
+            'AppPers':'',
+            'Auth':'',
+            'Owner':''
+        }
+    )
+    with open("test.html", "w") as f:
+        f.write(response.text)
+
 
 
 if __name__ == "__main__":
