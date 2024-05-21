979b94dc658b171bfd3ba65f5510b7202efa1880
Damir <damir@t1c.ru>
2024-05-21 18:53:55 +0300

    complete refactor for consistensy, FMAD is now ICID, more user-friendly

--- parse.py
+++ parse.py
@@ -1,23 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
 import itertools
 import json
 import datetime
+import requests
+from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 
-def get_table_by_fmad(fmad='01-01', lang='en'):
-    if lang not in ['en', 'ru', 'hy']:
-        raise Exception("Tried to request a table in non-supported language. Use 'en', 'ru' or 'hy'.")
-
-    response = requests.request(
+def aipo_request(icid_code : str = '', certificate_id : str = '', language : str = '') -> requests.Response:
+    return requests.request(
         'post',
-        'https://aipo.am/' + lang + '//ajax/search_mods_search_int_classification',
+        'https://aipo.am/' + language + '//ajax/search_mods_search_int_classification',
         data={
             'logic': 'partial',
-            'FMAD': fmad,
+            'FMAD': icid_code,
+            'Reg_num': certificate_id,
 
-            'Reg_num':'',
             'App_num':'',
             'App_date':'',
             'name':'',
@@ -26,73 +23,60 @@ def get_table_by_fmad(fmad='01-01', lang='en'):
             'Owner':''
         }
     )
-    return response
 
-def get_overview_by_fmad(fmad='01-01', lang='en'):
-    response = get_table_by_fmad(fmad, lang)
-    soup = BeautifulSoup(response.text, features="html.parser")
+def get_patent_by_id(id : int, language : str) -> dict:
+    response = aipo_request(certificate_id=id, language=language)
+    try:
+        application_id, title, certificate_id = BeautifulSoup(response.text, features="html.parser").find_all("td")
+    except ValueError:
+        return dict()
     
-    overview = {
-        "FMAD": fmad,
-        "data": []
-    }
-    for (application_id, title, certificate_id) in itertools.batched(soup.find_all("td"), n=3):
-        overview['data'].append({
-            "certificate_id": certificate_id.get_text(),
-            "application_id": application_id.get_text(),
-            "title": title.get_text(),
-            "patent_link": 'https://old.aipa.am/search_mods/industrial_design/view_item.php?id=' + certificate_id.get_text() + '&language=en'
-        })
-    return overview
-
-def generate_fmads():
-    fmads = []
-    for i in range(33): # 32 - amount of classes in fmad (mkpo, icid)
-        for j in range(17): # 16 - maximum second index in every class
-            fmads.append(str(i).zfill(2) + '-' + str(j).zfill(2))
-    return fmads
-
-def get_patent_by_id(id, lang="en"):
-    response = requests.request(
-        'post',
-        'https://aipo.am/' + lang + '//ajax/search_mods_search_int_classification',
-        data={
-            'logic': 'partial',
-            'Reg_num': str(id),
-
-            'FMAD': '',
-            'App_num':'',
-            'App_date':'',
-            'name':'',
-            'AppPers':'',
-            'Auth':'',
-            'Owner':''
-        }
-    )
-    soup = BeautifulSoup(response.text, features="html.parser")
-    application_id, title, certificate_id = soup.find_all("td")
     return {
         "certificate_id": certificate_id.get_text(),
         "application_id": application_id.get_text(),
         "title": title.get_text(),
-        "patent_link": 'https://old.aipa.am/search_mods/industrial_design/view_item.php?id=' + certificate_id.get_text() + '&language=en'
+        "patent_link": 'https://old.aipa.am/search_mods/industrial_design/view_item.php?id=' + \
+            certificate_id.get_text() + '&language=' + language
     }
 
-def download_remaining(downloaded_patents):
+def get_group_by_icid_code(icid_code : str, language : str):
+    response = aipo_request(icid_code=icid_code, language=language)
+    soup = BeautifulSoup(response.text, features="html.parser")
+    
+    group = {
+        "ICID_code": icid_code,
+        "data": []
+    }
+    for (application_id, title, certificate_id) in itertools.batched(soup.find_all("td"), n=3):
+        group['data'].append({
+            "certificate_id": certificate_id.get_text(),
+            "application_id": application_id.get_text(),
+            "title": title.get_text(),
+            "patent_link": 'https://old.aipa.am/search_mods/industrial_design/view_item.php?id=' + \
+                certificate_id.get_text() + '&language=' + language
+        })
+    return group
+
+def generate_icid_codes():
+    for i in range(33): # 32 - amount of classes in icids (mkpo, icid)
+        for j in range(17): # 16 - maximum second index in every class
+            yield str(i).zfill(2) + '-' + str(j).zfill(2)
+
+def fix_patents_list(downloaded_patents : list, languge : str):
     downloaded_patents.sort(key = lambda patent: int(patent["certificate_id"]))
     progress = tqdm(
-            enumerate(downloaded_patents, start=1), 
-            desc="Downloading remaining patents", 
-            total=int(downloaded_patents[-1]["certificate_id"])
-        )
+        enumerate(downloaded_patents, start=1), 
+        bar_format='{lbar}{bar:20}{r_bar}', 
+        total=int(downloaded_patents[-1]["certificate_id"])
+    )
     for (i, patent) in progress:
         next_certificate = int(patent["certificate_id"])
         if i == next_certificate:
-            progress.set_description(f"Checking")
+            progress.set_description(f"Correct id={i}")
             continue
         elif i < next_certificate:
             progress.set_description(f"Missing id={i}")
-            missing = get_patent_by_id(id=i)
+            missing = get_patent_by_id(id=i, language=languge)
             downloaded_patents.insert(i - 1, missing)
             i += 1
             continue
@@ -100,62 +84,81 @@ def download_remaining(downloaded_patents):
             progress.set_description(f"Dublicate id={i}")
             downloaded_patents.pop(i - 1)
     
-    progress = tqdm(
-            enumerate(downloaded_patents, start=1), 
-            desc="Confirming every certificate id is present once", 
-            total=int(downloaded_patents[-1]["certificate_id"])
-        )
+    print("Confirming every id is downloaded and unique... ", end="")
     for (i, patent) in progress:
         next_certificate = int(patent["certificate_id"])
         if i != next_certificate:
             raise ValueError(f"Error at id={i}")
-    
+    print("Done!")
+
     return downloaded_patents
 
-def download_patents(all : bool):
-    if all:
-        certificate_ids = {
-            "parsing_date": datetime.date.today().isoformat(),
-            "entries": 0,
-            "overviews": []
-        }
-
-        for fmad in tqdm(generate_fmads(), desc="Downloading every entry with FMAD"):
-            overview = get_overview_by_fmad(fmad, 'en')
-            if overview['data']:
-                certificate_ids["entries"] += len(overview["data"])
-                certificate_ids['overviews'].append(overview)
-        
-        with open("./data/en/data.json", "w", encoding='utf-8') as f:
-            f.write(json.dumps(certificate_ids, ensure_ascii=False))
-    else:
-        with open("./data/en/data.json", "r", encoding='utf-8') as f:
-            certificate_ids = json.load(f)
-            
-    almost_all_patents = []
-    for overview in tqdm(certificate_ids["overviews"], desc="Extracting individual patents"):
-        for patent in overview["data"]:
-            almost_all_patents.append(patent)
+def get_ICID_json(language : str):
+    print("Downloading every entry with ICID")
+    certificate_ids = {
+        "parsing_date": datetime.date.today().isoformat(),
+        "entries": 0,
+        "icid_code_groups": []
+    }
+    progress = tqdm(generate_icid_codes(), bar_format='{lbar}{bar:20}{r_bar}', total=17*33)
+    for icid_code in progress:
+        progress.set_description(icid_code)
+        group = get_group_by_icid_code(icid_code=icid_code, language=language)
+        if group['data']:
+            certificate_ids["entries"] += len(group["data"])
+            certificate_ids['icid_code_groups'].append(group)
     
-    all_patents = download_remaining(almost_all_patents)
-    print(f"Every patent up to {certificate_ids["parsing_date"]} is succesfully downloaded in ./data/en/patents.json")
+    with open("./data/" + language + "/data.json", "w", encoding='utf-8') as f:
+        f.write(json.dumps(certificate_ids, ensure_ascii=False))
 
-    with open("./data/en/patents.json", "w", encoding='utf-8') as f:
+
+def get_all_patents(language : str):
+    path = "./data/" + language + "/"
+    try:
+        with open(path + "ICID.json", "r", encoding='utf-8') as f:
+            certificate_ids = json.load(f)
+    except FileNotFoundError:
+        print(f"Missing {path + "ICID.json"}\nDo you want to get it? [y/n]")
+        if input(">>>").strip().lower() == "y":
+            get_ICID_json(language)
+        else:
+            return
+
+        with open(path + "ICID.json", "r", encoding='utf-8') as f:
+            certificate_ids = json.load(f)
+             
+    ready_patents = []
+    for group in certificate_ids["icid_code_groups"]:
+        for patent in group["data"]:
+            ready_patents.append(patent)
+    
+    all_patents = fix_patents_list(ready_patents, languge=language)
+    print(f"Every patent up to {certificate_ids["parsing_date"]} is succesfully downloaded in {path}patents.json")
+    print(f"In total: {all_patents[-1]["certificate_id"]} unique patents")
+
+    with open(path + "patents.json", "w", encoding='utf-8') as f:
         f.write(json.dumps(all_patents, ensure_ascii=False))
 
 def main():
+    print("Select language (default = en) [ en | ru | hy | all ]")
+    language = input(">>>").strip().lower() or "en"
+    if language not in ["en", "ru", "hy", "all"]:
+        raise ValueError(f"'{language}' is not a supported language")
+    
+    query = ["en", "ru", "hy"] if language == "all" else [language]
+    
     print("What do you want to do?")
-    print("1: Download all certificate ids with links to the patents")
-    print("2: download only patents without FMAD (needs 1st option to be done)")
-    match int(input()):
+    print("1: Get only ICID.json (patents with ICID id)")
+    print("2: Get all patents in patents.json (will require step one done)")
+    match int(input(">>>")):
         case 1:
-            download_patents(all=True)
+            for language in query:
+                get_ICID_json(language)
         case 2:
-            download_patents(all=False)
+            for language in query:
+                get_all_patents(language)
         case _:
             print("No such option")
-    
-
 
 if __name__ == "__main__":
     main()
